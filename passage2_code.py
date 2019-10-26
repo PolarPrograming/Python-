9e15ea3c7b8077663b09d0ed248f15ed9084f7ff
Stanford-Peng <48232605+Stanford-Peng@users.noreply.github.com>
2019-10-26 16:10:55 +1100

    Add files via upload

---
+++ passage2_code.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 13 21:52:38 2019
+
+@author: stanford
+"""
+import random
+
+#1
+#num = random.randint(1,101)
+#guess = 0
+#while num != guess:
+#    guess = int(input("To guess a number, please enter a number which is between 0 and 100: "))
+#    if guess > num:
+#        print("Guessed number is too big")
+#    elif guess < num:
+#        print("Guessed number is too small")
+#    else:
+#        print("You have got it! the number is {}".format(num))
+
+#2
+num = random.randint(1,101)
+guess = 0
+times = input("Please enter the maximum number of times you want to guess the number for: ")
+while not times.isdigit():
+    times = input("Please enter the maximum number of times you want to guess the number for: ")
+    
+for i in range(0,int(times)):
+    if guess != num:
+        guess = input("To guess a number, please enter a number which is between 1 and 100(inclusive): ")
+        while not guess.isdigit() : 
+            print("please enter a valid number")     
+        guess = int(guess)
+        if guess > num:
+            print("Guessed number is too big")
+        elif guess < num:
+            print("Guessed number is too small")
+        else:
+            #str = "shot" if i == 1 else str = "shots"
+            print("You have got it! the number is {} and you have tried {} {}".format(num, i, "shot" if i == 1 else "shots"))
+            break
+    if i == int(times) - 1 and guess != num:
+        print("You did not won and you have used up your chances.")
+    
+
+        
+        
+    
