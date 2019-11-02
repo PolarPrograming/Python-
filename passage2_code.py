5017353aa37f344d3f7f01231825603d9b6b5d88
Stanford-Peng <48232605+Stanford-Peng@users.noreply.github.com>
2019-11-02 14:48:31 +1100

    Add files via upload

--- passage2_code.py
+++ passage2_code.py
@@ -5,7 +5,7 @@ Created on Sun Oct 13 21:52:38 2019
 
 @author: stanford
 """
-import random
+
 
 #1
 #num = random.randint(1,101)
@@ -20,28 +20,29 @@ import random
 #        print("You have got it! the number is {}".format(num))
 
 #2
-num = random.randint(1,101)
-guess = 0
+import random
+num = random.randint(1,100)
+#guess = 0
 times = input("Please enter the maximum number of times you want to guess the number for: ")
-while not times.isdigit():
+while not times.isdigit() or int(times) <= 0:
     times = input("Please enter the maximum number of times you want to guess the number for: ")
     
 for i in range(0,int(times)):
-    if guess != num:
+    guess = input("To guess a number, please enter a number which is between 1 and 100(inclusive): ")
+    while not guess.isdigit() or int(guess) > 100 or int(guess) <= 0 : 
+        print("please enter a valid number")
         guess = input("To guess a number, please enter a number which is between 1 and 100(inclusive): ")
-        while not guess.isdigit() : 
-            print("please enter a valid number")     
-        guess = int(guess)
-        if guess > num:
-            print("Guessed number is too big")
-        elif guess < num:
-            print("Guessed number is too small")
-        else:
-            #str = "shot" if i == 1 else str = "shots"
-            print("You have got it! the number is {} and you have tried {} {}".format(num, i, "shot" if i == 1 else "shots"))
-            break
-    if i == int(times) - 1 and guess != num:
-        print("You did not won and you have used up your chances.")
+    guess = int(guess)
+    if guess > num:
+        print("Guessed number is too big")
+    elif guess < num:
+        print("Guessed number is too small")
+    else:
+        #str = "shot" if i == 1 else str = "shots"
+        print("You have got it! the number is {} and you have tried {} {}".format(num, i + 1, "shot" if i + 1 == 1 else "shots"))
+        break
+if i == int(times) - 1 and guess != num:
+    print("You did not won and you have used up your chances.")
     
 
         
